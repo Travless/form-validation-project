e43bc6140d165c7e7c793b2446522e0c0c7489a0
Travless <travis.white0101@gmail.com>
2023-01-04 14:12:41 -0500

    adjust approach of event listener to focus on form submission, and remove standard form validation via novalidate with the html form element

--- app.js
+++ app.js
@@ -8,20 +8,94 @@ const username = document.getElementById('username');
 const password = document.getElementById('password');
 const passwordConf = document.getElementById('password-conf');
 
-firstName.addEventListener('input', e => {
+form.addEventListener('submit', e=> {
     e.preventDefault();
 
-    if(firstName.validity.tooShort) {
-        firstName.setCustomValidity('Please enter full, legal first name (no nicknames or abbreviations)');
-    } else {
-        firstName.setCustomValidity('');
-    }
-});
+    validateInputs();
+})
 
-lastName.addEventListener('input', e => {
-    if(lastName.validity.tooShort){
-        lastName.setCustomValidity('Please enter full, legal last name (no nicknames or abbreviations');
+const setError = (element, message) => {
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.error');
+
+    errorDisplay.innerText = message;
+    inputControl.classList.add('error');
+    inputControl.classList.remove('success');
+}
+
+const setSuccess = (element) => {
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.error');
+
+    errorDisplay.innerText = '';
+    inputControl.classList.add('success');
+    inputControl.classList.remove('error');
+}
+
+const isEmailValid = (email) => {
+    const regExpEmail = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return regExpEmail.test(String(email).toLowerCase());
+}
+
+const validateInputs = () => {
+    const firstNameValue = firstName.value.trim();
+    const lastNameValue = lastName.value.trim();
+    const countryValue = country.value.trim();
+    const zipCodeValue = zipCode.value.trim();
+    const emailValue = email.value.trim();
+    const usernameValue = username.value.trim();
+    const passwordValue = password.value.trim();
+    const passwordConfValue = passwordConf.value.trim();
+
+    if(firstNameValue === ''){
+        setError(firstName, 'First name is required')
     } else {
-        lastName.setCustomValidity('');
+        setSuccess(firstName);
     }
-});
+
+    if(lastNameValue === ''){
+        setError(lastName, 'Last name is required')
+    } else {
+        setSuccess(lastName);
+    }
+
+    if(countryValue === 'null'){
+        setError(country, 'Country is required')
+    } else {
+        setSuccess(country);
+    }
+
+    if(zipCodeValue === ''){
+        setError(zipCode, 'Zip code is required')
+    } else if(zipCodeValue.length<5 || zipCodeValue.legnth>5){
+        setError(zipCode, 'Please enter a valid US zip code')
+    } else {
+        setSuccess(zipCode);
+    }
+
+    if(emailValue === ''){
+        setError(email, 'Email address is required')
+    } else if(!isEmailValid(emailValue)) {
+        setError(email, 'Please provide a valid email address')
+    } else {
+        setSuccess(email);
+    }
+
+    if(usernameValue === ''){
+        setError(username, 'Username is required')
+    } else {
+        setSuccess(username);
+    }
+
+    if(passwordValue === ''){
+        setError(password, 'Password is required')
+    } else {
+        setSuccess(password);
+    }
+
+    if(passwordConfValue === ''){
+        setError(passwordConf, 'Password confirmation is required')
+    } else {
+        setSuccess(passwordConf);
+    }
+};
